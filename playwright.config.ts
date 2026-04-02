19e6af34a7ab432ffb13c2f3289fdc267045e140
emannaji597 <enaji4643@gmail.com>
2026-04-02 22:50:44 +0300

    🐛 FIX: Remove headless

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
 
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://todo.qacart.com',
-    headless: false,
+    
     
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
